96f9898abacb91a773d17329298ddfcfead70d59
Aaron Stannard <aaron@petabridge.com>
2025-01-03 08:56:36 -0600

    Initial commit (#1)
    * initial commit
    
    * added Dependabot
    
    * initial commit

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+#### 0.1.0 Januard 3rd 2025 ####
+
+Initial release
---
+++ src/LinkValidator/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkValidator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
